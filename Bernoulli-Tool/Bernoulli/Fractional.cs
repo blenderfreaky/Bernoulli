b4b4002d0f434a33dac7cf371cf9969cfefa10c2
blenderfreaky <blendcrafterxiv@gmail.com>
2019-06-02 20:40:39 +0200

    Fixed Tool

--- Bernoulli-Tool/Bernoulli/Fractional.cs
+++ Bernoulli-Tool/Bernoulli/Fractional.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Bernoulli
 {
     public class Fractional
     {
-        public long numerator;
-        public long denominator;
-        public decimal Value => numerator / denominator;
+        public BigInteger numerator;
+        public BigInteger denominator;
 
-        public Fractional(long numerator, long denominator)
+        public Fractional(BigInteger numerator, BigInteger denominator)
         {
             this.numerator = numerator;
             this.denominator = denominator;
         }
 
         public static Fractional operator +(Fractional left, Fractional right) =>
-            new Fractional(left.numerator*right.denominator + right.numerator*left.denominator, left.denominator * right.denominator).Shorten();
+            new Fractional(left.numerator*right.denominator + right.numerator*left.denominator, left.denominator * right.denominator);
 
-        public static Fractional operator -(Fractional value) => new Fractional(-value.numerator, value.denominator);
+        public static Fractional operator -(Fractional value) => new Fractional(value.numerator, value.denominator);
 
         public static Fractional operator -(Fractional left, Fractional right) =>
             left + (-right);
 
         public static Fractional operator *(Fractional left, Fractional right) =>
-            new Fractional(left.numerator * right.numerator, left.denominator * right.denominator).Shorten();
+            new Fractional(left.numerator * right.numerator, left.denominator * right.denominator);
 
         public static Fractional operator /(Fractional left, Fractional right) =>
-            new Fractional(left.numerator * right.denominator, left.denominator * right.numerator).Shorten();
+            new Fractional(left.numerator * right.denominator, left.denominator * right.numerator);
 
 
-        private static long GCD(long a, long b)
+        private static BigInteger GCD(BigInteger a, BigInteger b)
         {
-            a = Math.Abs(a);
-            b = Math.Abs(b);
+            a *= a.Sign;
+            b *= b.Sign;
 
             while (a != 0 && b != 0)
             {
@@ -47,7 +47,7 @@ namespace Bernoulli
 
         public Fractional Shorten() => Shorten(GCD(numerator, denominator));
 
-        public Fractional Shorten(long factor) => new Fractional(numerator / factor, denominator / factor) * Math.Sign(denominator);
+        public Fractional Shorten(BigInteger factor) => new Fractional(numerator / factor * denominator.Sign, denominator / factor * denominator.Sign);
 
         public static implicit operator Fractional(long val) => new Fractional(val, 1);
 
--- Bernoulli-Tool/Bernoulli/Program.cs
+++ Bernoulli-Tool/Bernoulli/Program.cs
@@ -6,23 +6,23 @@ namespace Bernoulli
     {
         static void Main(string[] args)
         {
-            BernoulliNumbers();
+            BernoulliNumbers(100000);
         }
 
-        private static void BernoulliNumbers()
+        private static void BernoulliNumbers(int amount)
         {
-            Fractional[] bernoulli = new Fractional[100];
+            Fractional[] bernoulli = new Fractional[amount];
             bernoulli[0] = 1;
             Console.WriteLine($"B_{0}: {bernoulli[0]}");
 
-            for (long n = 1; n < 50; n++)
+            for (long n = 1; n < amount; n++)
             {
                 Fractional currentSum = 0;
                 for (long k = 0; k < n; k++)
                 {
                     currentSum += Choose(n + 1, k) * bernoulli[k];
                 }
-                bernoulli[n] = -currentSum / Choose(n + 1, n);
+                bernoulli[n] = (-currentSum / Choose(n + 1, n)).Shorten();
 
                 Console.WriteLine($"B_{n}: {bernoulli[n]}");
             }
