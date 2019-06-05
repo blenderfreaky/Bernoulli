af400630daf2d9c21190b9d7d0b2249b80148d0d
funketh <45576110+funketh@users.noreply.github.com>
2019-06-05 09:31:25 +0200

    Update bernoulli.py

--- bernoulli.py
+++ bernoulli.py
@@ -1,6 +1,5 @@
 import abc
 import math
-from fractions import Fraction
 from decimal import Decimal
 from matplotlib import pyplot as plt
 from scipy import integrate
@@ -24,17 +23,34 @@ class LazyList:
 class Bernoulli(LazyList):
     def __init__(self):
         super().__init__()
-        self.sequence = [Fraction(1)]
+        self.sequence = [Decimal(1)]
 
     def _calculate_next(self):
         n = len(self.sequence)
-        bn = -1 * (sum(Fraction(binom(n+1, k)) * self.sequence[k]
+        bn = -1 * (sum(Decimal(binom(n+1, k)) * self.sequence[k]
                        for k in range(n))
-                   / Fraction(binom(n+1, n)))
+                   / Decimal(binom(n+1, n)))
         self.sequence.append(bn)
 
     def plot(self):
-        plt.plot(tuple(range(len(self.sequence))), self.sequence)
+        seq = []
+        for i, x in enumerate(self.sequence):
+            if i % 2 == 1 or i < 3:
+                seq.append(0)
+            else:
+                seq.append(x)
+        sseq = logscale(seq)
+        ax = plt.subplot()
+        ax.plot(sseq, 'bx')
+        ax.plot(sseq, 'r-', linewidth=0.4)
+        plt.yticks(sseq[::10], ['%.1E' % x for x in seq[::10]])
+        ax.legend([r"$B_n$"])
+        plt.xlabel('x')
+        plt.ylabel('y')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
         plt.show()
 
 
@@ -44,14 +60,36 @@ class BernoulliPolynomial:
         self.bern = bern
 
     def __call__(self, x):
-        return sum(Fraction(binom(self.n, k)) * self.bern[k] * x**(self.n-k)
+        return sum(Decimal(binom(self.n, k))
+                     * self.bern[k]
+                     * Decimal(x**(self.n-k))
                    for k in range(self.n+1))
 
 
 class PeriodicalBernoulliPolynomial(BernoulliPolynomial):
     def __call__(self, x):
-        return super().__call__(x - (int(x) if int(x) < x else int(x)-1))
+        return super().__call__(x - (int(x) if int(x) <= x else int(x)-1))
 
+    def plot(self, accuracy = 1000):
+        data = [self(x / accuracy)
+                for x in range(-10 * accuracy, 10 * accuracy + 1)]
+        x = [x / accuracy
+             for x in range(-10 * accuracy, 10 * accuracy + 1)]
+        ax = plt.subplot()
+        for i in range(0, len(data), accuracy):
+            ax.plot(x[i:i+accuracy], data[i:i+accuracy], 'b')
+        ax.plot([n for n in range(-10, 10)], [-0.5]*20, 'go',
+                markersize=5.0)
+        ax.plot([n for n in range(-9, 11)], [0.5]*20, 'ro',
+                markersize=5.0, markerfacecolor='white')
+        ax.legend([f"$P_{self.n}(x)$"])
+        plt.xlabel('x')
+        plt.ylabel('y')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        plt.show()
 
 def gamma(n, m, bern = Bernoulli()):
     Hn = sum(1/k for k in range(1, n+1))
@@ -60,10 +98,22 @@ def gamma(n, m, bern = Bernoulli()):
     return Hn - math.log(n) - 1/(2*n) + bsum - pintegral
 
 
-def test_bern():
+def logscale(seq):
+    res = []
+    for x in seq:
+        x = Decimal(x)
+        if x > 0:
+            res.append(math.log(x+1))
+        else:
+            res.append(-math.log(abs(x)+1))
+    return res
+
+
+def test_bern(n=10):
     b = Bernoulli()
-    for i in range(10):
-        print(b[i])
+    for i, n in enumerate(range(n)):
+        b[n]
+        print(i)
     b.plot()
 
 def test_bern_pol():
@@ -75,8 +125,11 @@ def test_periodical_bern_pol():
     a, b, c = pbp(0.75), pbp(3.75), pbp(-7.25)
     print(f"P(0.75): {a}; P(3.75): {b}; P(-7.25): {c}")
     assert a == b == c
+    pbp.plot()
 
 def test_gamma():
     y = gamma(n=10, m=3)
     print(y)
 
+if __name__ == '__main__':
+    test_periodical_bern_pol()
